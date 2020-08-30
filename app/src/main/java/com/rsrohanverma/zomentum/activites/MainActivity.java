b8d2ff3acdcddaeb4180f1efa380164fac512088
Rohan Verma <rsrohanverma@gmail.com>
2020-08-30 16:12:19 +0530

    Initial commit

---
+++ app/src/main/java/com/rsrohanverma/zomentum/activites/MainActivity.java
@@ -0,0 +1,16 @@
+package com.rsrohanverma.zomentum.activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rsrohanverma.zomentum.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
