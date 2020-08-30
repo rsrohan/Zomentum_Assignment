1ba4620f3dd4a9b796aad80a5bcbf3623fe63747
Rohan Verma <rsrohanverma@gmail.com>
2020-08-30 16:19:43 +0530

    Chat Screen UI Added

---
+++ app/src/main/java/com/rsrohanverma/zomentum/activites/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.rsrohanverma.zomentum.activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rsrohanverma.zomentum.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
